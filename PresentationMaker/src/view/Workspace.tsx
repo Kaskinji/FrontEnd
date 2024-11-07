53e3d59e447406201ddd7b40fbcdf12d25d7cf93
Никита Касаткин <nk1kasatkin@yandex.ru>
2024-11-07 17:43:48 +0300

    lw3

--- PresentationMaker/src/view/Workspace.tsx
+++ PresentationMaker/src/view/Workspace.tsx
@@ -1,5 +1,11 @@
-function Workspace() {
-    return (
-      
-    );
-  }
+import { SlideType } from "../entities/Presentation";
+
+type WorkspaceProps = {
+  slide: SlideType;
+};
+
+function Workspace({ slide }: WorkspaceProps) {
+  return <Slide />;
+}
+
+export { Workspace };
